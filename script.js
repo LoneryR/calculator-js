9fed052d99f47b4423f7c5adccf55582cb26f580
max <max.dobrikR@gmail.com>
2022-09-05 20:54:25 +0300

    added logic js

---
+++ script.js
@@ -0,0 +1,31 @@
+const prev = document.querySelector('.app__output_num-prev')
+const now = document.querySelector('.app__output_num-now')
+
+const operators = Array.from(document.querySelectorAll('.operator'))
+
+console.log(operators)
+
+operators.map((button) => {
+	button.addEventListener('click', (e) => {
+		switch(e.target.innerText){
+			case 'C':
+				prev.innerText = ''
+				now.innerText = ''
+				break
+			case 'CE':
+				if(now.innerText){
+					now.innerText = now.innerText.slice(0, -1)
+				}
+				break
+			case '=':
+				prev.innerText = now.innerText
+				now.innerText = eval(now.innerText)
+				break
+			default: 
+				if(now.innerText == 0){
+					now.innerText = ''
+				}
+				now.innerText += e.target.innerText
+		}
+	})
+})
